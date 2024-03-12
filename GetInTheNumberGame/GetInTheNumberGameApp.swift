11b711f282207e538cafba4f25cad0983f2e5707
Evgeni Glushko <sstawr@gmail.com>
2024-03-12 16:55:44 +0300

    Initial Commit

---
+++ GetInTheNumberGame/GetInTheNumberGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetInTheNumberGameApp.swift
+//  GetInTheNumberGame
+//
+//  Created by Evgeni Glushko on 12.03.24.
+//
+
+import SwiftUI
+
+@main
+struct GetInTheNumberGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
